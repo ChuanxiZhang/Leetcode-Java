f1b8099fe5667cbef7a1348965f621f021857377
ChuanxiZhang <chuanxiz@usc.edu>
2017-04-18 19:16:43 -0700

    V1.0002
    V1.0002

--- src/test.java
+++ src/test.java
@@ -3,7 +3,8 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		int ¹ù=1;
+		System.out.println(¹ù);
 	}
 
 }
